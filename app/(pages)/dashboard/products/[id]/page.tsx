3bf7470cee371b346f753430c0aa0a317e7c6fc4
Hilario Napoles <hilario.napoles@gmail.com>
2025-02-17 11:50:40 -0600

    more changes

--- app/(pages)/dashboard/products/[id]/page.tsx
+++ app/(pages)/dashboard/products/[id]/page.tsx
@@ -30,14 +30,6 @@ export default async function ProductEditPage({ params }: {
 
     const categories = lookup2.data; 
 
-    console.log(product)
-    console.log(categories)
-
-    /*
-    return (
-        <ProductEditForm product={product} types={types} categories={categories} />
-    )
-    */
     return (
       <ProductUpdate product={product} types={types} categories={categories} />
     )
