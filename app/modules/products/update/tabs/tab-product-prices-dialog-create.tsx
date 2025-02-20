00d3f8ca60ed0772fb3b66072d12a9733f55ac1f
Hilario Napoles <hilario.napoles@gmail.com>
2025-02-20 14:49:27 -0600

    more changes

--- app/modules/products/update/tabs/tab-product-prices-dialog-create.tsx
+++ app/modules/products/update/tabs/tab-product-prices-dialog-create.tsx
@@ -9,14 +9,82 @@ import {
   DialogTitle,
   //DialogTrigger,
 } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
+
 import { Label } from '@/components/ui/label';
 
+import Link from 'next/link';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { toast } from '@/hooks/use-toast';
 import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Input } from '@/components/ui/input';
+
+import { revalidateAndRedirectUrl } from '@/lib/revalidate-path';
+
+import {
+  ZodSchemaProduct,
+  ProductData,
+  ProductSellingPricesData,
+} from '@/app/model/products-model';
+import { Lookup } from '@/app/model/lookups-model';
+import { UpdateProduct } from '@/app/action/server/products-actions';
 
 import { useGlobalStore } from '@/app/provider/zustand-provider';
 
+const defaultValues: ProductSellingPricesData = {
+  _id: '',
+  productId: '',
+  orderType: '',
+  storeName: '',
+  sellingPrice: 0.0,
+};
+
 export default function TabProductPricesDialogCreate() {
+  const form = useForm<ProductData>({
+    resolver: zodResolver(ZodSchemaProduct),
+    defaultValues: defaultValues,
+    mode: 'onBlur',
+  });
+
+  const {
+    formState: {
+      //errors,
+      isSubmitting,
+    },
+  } = form;
+
+  async function onSubmit(data: ProductData) {
+    delete data._id;
+    //const productCreated = await CreateProduct(data);
+
+    toast({
+      title: 'Data saved',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{}</code>
+        </pre>
+      ),
+    });
+    revalidateAndRedirectUrl('/dashboard/products');
+  }
+
   const product = useGlobalStore((state) => state.product);
 
   const { isCreateDialogOpen, closeCreateDialog, toggleCreateDialog } =
@@ -35,36 +103,53 @@ export default function TabProductPricesDialogCreate() {
             {`Add product prices for product id[${product?._id}], name[${product?.name}]`}
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              value="Pedro Duarte"
-              className="col-span-3"
-              onChange={(e) => console.log(e)}
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price</FormLabel>
+                  <Input
+                    type="number"
+                    step="0.01" // Allows decimals
+                    placeholder="0.00"
+                    value={field.value ? Number(field.value).toFixed(2) : ''}
+                    onChange={(e) => {
+                      const value = e.target.value; // Always a string from input
+                      const numericValue = parseFloat(value);
+
+                      // Ensure valid numeric input
+                      if (!isNaN(numericValue)) {
+                        field.onChange(numericValue.toFixed(2)); // Pass as string
+                      } else {
+                        field.onChange(''); // Handle empty input
+                      }
+                    }}
+                  />
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              value="@peduarte"
-              className="col-span-3"
-              onChange={(e) => console.log(e)}
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button onClick={closeCreateDialog}>Cancel</Button>
-        </DialogFooter>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+            <div className="mt-6 flex justify-end gap-4">
+              <Link
+                href="/dashboard/products/create"
+                className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+              >
+                Cancel
+              </Link>
+              <Button type="submit" disabled={isSubmitting}>
+                {isSubmitting ? 'Submitting...' : 'Submit'}
+              </Button>
+            </div>
+          </form>
+        </Form>
+
+        <DialogFooter></DialogFooter>
       </DialogContent>
     </Dialog>
   );
