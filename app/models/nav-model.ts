c7ad1c300b22708a6e53ecbc44f00974c4b60476
Hilario Napoles <hilario.napoles@gmail.com>
2025-02-22 15:19:47 -0600

    more changes

--- app/models/nav-model.ts
+++ app/models/nav-model.ts
@@ -1,52 +1,55 @@
-import { LucideIcon } from "lucide-react";
+import { LucideIcon } from 'lucide-react';
 
 import {
-    Home,
-    LineChart,
-    Package,
-    //Package2,
-    //PanelLeft,
-    //Settings,
-    ShoppingCart,
-    Users2
-  } from 'lucide-react';
-
+  Home,
+  LineChart,
+  Package,
+  //Package2,
+  //PanelLeft,
+  //Settings,
+  ShoppingCart,
+  Users2,
+} from 'lucide-react';
 
 export interface NavItems {
-    title: string,
-    href: string,
-    iconName: LucideIcon
+  title: string;
+  href: string;
+  iconName: LucideIcon;
 }
-  
+
 export interface NavItemsProps {
-    navItems: NavItems[]
+  navItems: NavItems[];
 }
-  
 
 export const listNavItems = [
-    {
-        title: "Dashboard",
-        href: "/dashboard",
-        iconName: Home
-    },
-    {
-      title: "Orders",
-      href: "/dashboard/orders",
-      iconName: ShoppingCart
-    },
-    {
-      title: "Products",
-      href: "/dashboard/products",
-      iconName: Package
-    },
-    {
-      title: "Customers",
-      href: "/dashboard/customers",
-      iconName: Users2
-    },
-    {
-      title: "Analytics",
-      href: "/dashboard/analytics",
-      iconName: LineChart
-    }
-]
+  {
+    title: 'Dashboard',
+    href: '/dashboard',
+    iconName: Home,
+  },
+  {
+    title: 'Orders',
+    href: '/orders',
+    iconName: ShoppingCart,
+  },
+  {
+    title: 'Products',
+    href: '/products',
+    iconName: Package,
+  },
+  {
+    title: 'Customers',
+    href: '/customers',
+    iconName: Users2,
+  },
+  {
+    title: 'Stores',
+    href: '/stores',
+    iconName: Users2,
+  },
+  {
+    title: 'Analytics',
+    href: '/analytics',
+    iconName: LineChart,
+  },
+];
