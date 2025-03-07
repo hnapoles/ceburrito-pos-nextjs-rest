76d75ec6d8fd3b14e4f967f0c7717631f19c75cc
Hilario Napoles <hilario.napoles@gmail.com>
2025-03-07 08:09:47 -0600

    closes #29, closes #32

--- app/(pages)/orders/[id]/edit/page.tsx
+++ app/(pages)/orders/[id]/edit/page.tsx
@@ -1,25 +1,63 @@
-//import { Loader2 } from 'lucide-react';
+import { GetOrderById } from '@/app/actions/server/orders-actions';
 import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card-rounded-sm';
+  GetLookupCustomers,
+  GetLookups,
+} from '@/app/actions/server/lookups-actions';
+import NotFoundGlobal from '@/app/nav/not-found-global';
 
-export default function Loading() {
+import OrdersIdBase from '@/app/features/orders/id/orders-id-base';
+
+//start of function
+export default async function OrdersByIdEditPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const id = (await params).id;
+
+  const order = await GetOrderById(id);
+  const lookups = await GetLookups('order', null);
+
+  const statusesLookup = lookups.data.filter(
+    (item) => item.lookupCode === 'status',
+  );
+
+  const orderTypesLookup = lookups.data.filter(
+    (item) => item.lookupCode === 'type',
+  );
+
+  const { data: dineModes } = await GetLookups(order.type || '', 'dineMode');
+  const { data: paymentMethods } = await GetLookups('order', 'paymentMethod');
+  const { data: statuses } = await GetLookups('order', 'status');
+
+  if (!order) {
+    return (
+      <NotFoundGlobal display={'Order data not found'} backUrl={'/orders'} />
+    );
+  }
+
+  console.log('order data from the server ', order);
+
+  const customers = await GetLookupCustomers();
+
+  //if-testing - set to true
+  if (false)
+    return (
+      <>
+        <div>Testing...</div>
+        <pre>order data: {JSON.stringify(order, null, 2)}</pre>
+        <pre>statuses: {JSON.stringify(statusesLookup, null, 2)}</pre>
+        <pre>customersLookup: {JSON.stringify(customers, null, 2)}</pre>
+        <pre>orderTypesLookup: {JSON.stringify(orderTypesLookup, null, 2)}</pre>
+      </>
+    );
+
   return (
-    <div className="grid gap-4">
-      {/* Left Side Skeleton */}
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            <div className="h-6 w-40 bg-gray-200 animate-pulse rounded-md"></div>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="h-80 w-full bg-gray-200 animate-pulse rounded-md"></div>
-        </CardContent>
-      </Card>
-    </div>
+    <OrdersIdBase
+      orderData={order}
+      dineModes={dineModes}
+      paymentMethods={paymentMethods}
+      statuses={statuses}
+    />
   );
 }
