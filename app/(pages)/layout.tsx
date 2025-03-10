bd024877790c5d82f1a5421dd690cbae7ab778ad
Hilario Napoles <hilario.napoles@gmail.com>
2025-03-10 09:52:00 -0500

    more screen fixes

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -18,7 +18,7 @@ export default function DashboardDefaultLayout({
     <NavProvider>
       <main className="flex min-h-screen w-full flex-col bg-muted/40">
         <DesktopNav />
-        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <div className="flex flex-col gap-1 lg:gap-1 md:py-1 py-1 sm:pl-14">
           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <MobileNav />
             <PagesBreadCrumb />
