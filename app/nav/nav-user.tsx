f491657875bc67e924a9e7b0e990216e433d65c3
Hilario Napoles <hilario.napoles@gmail.com>
2025-03-13 16:51:14 -0500

    allow change store

--- app/nav/nav-user.tsx
+++ app/nav/nav-user.tsx
@@ -10,51 +10,64 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
+import { MessageCircleQuestion, Settings } from 'lucide-react';
 
 export async function User() {
   const session = await auth();
   const user = session?.user;
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-          className="overflow-hidden rounded-full"
-        >
-          <Image
-            src={user?.image ?? '/placeholder-user.jpg'}
-            width={36}
-            height={36}
-            alt="Avatar"
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
             className="overflow-hidden rounded-full"
-          />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Settings</DropdownMenuItem>
-        <DropdownMenuItem>Support</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        {user ? (
+          >
+            <Image
+              src={user?.image ?? '/placeholder-user.jpg'}
+              width={36}
+              height={36}
+              alt="Avatar"
+              className="overflow-hidden rounded-full"
+            />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <Separator />
           <DropdownMenuItem>
-            <form
-              action={async () => {
-                'use server';
-                await signOut();
-              }}
-            >
-              <button type="submit">Sign Out</button>
-            </form>
+            <Link href="/stores/change">Change Store</Link>
           </DropdownMenuItem>
-        ) : (
+          <DropdownMenuSeparator />
           <DropdownMenuItem>
-            <Link href="/login">Sign In</Link>
+            <Settings /> Settings
           </DropdownMenuItem>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuItem>
+            <MessageCircleQuestion /> Support
+          </DropdownMenuItem>
+
+          <DropdownMenuSeparator />
+          {user ? (
+            <DropdownMenuItem>
+              <form
+                action={async () => {
+                  'use server';
+                  await signOut();
+                }}
+              >
+                <button type="submit">Sign Out</button>
+              </form>
+            </DropdownMenuItem>
+          ) : (
+            <DropdownMenuItem>
+              <Link href="/login">Sign In</Link>
+            </DropdownMenuItem>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 }
